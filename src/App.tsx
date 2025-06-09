28fdd30264202c9b6d3357900feaf63683b812a3
Instabidsai <justin@instabids.ai>
2025-06-09 11:38:19 -0400

    Update App.tsx with sales bot routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import SalesBotLanding from "./pages/tools/SalesBotLanding";
+import SalesBot from "./pages/tools/SalesBot";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +18,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          {/* Sales Bot Routes */}
+          <Route path="/sales-bot" element={<SalesBotLanding />} />
+          <Route path="/tools/sales-bot" element={<SalesBot />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
